996890540e8621dcf5f97b880422b44e08d425b8
Rahi <p.rahifb@gmail.com>
2019-07-12 21:05:27 +0530

    Intent Calculator

--- app/src/main/java/com/rahi/demo/MainActivity.java
+++ app/src/main/java/com/rahi/demo/MainActivity.java
@@ -11,48 +11,35 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button b1,b2;
-    TextView tv1;
-    EditText et1,et2;
+    Button b1;
+    int s1;
+    EditText et1;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        b2=findViewById(R.id.btn_calc);
+
         b1=findViewById(R.id.btn_show);
-        tv1=findViewById(R.id.tv_val);
+
         et1=findViewById(R.id.et_name);
-        et2=findViewById(R.id.et_number);
+
 
 
        b1.setOnClickListener(new View.OnClickListener() {
            @Override
            public void onClick(View v) {
-               try {
-                   int s1 = Integer.parseInt(et1.getText().toString());
-                   int s2 = Integer.parseInt(et2.getText().toString());
-                   int sum=s1+s2;
-                   tv1.setText("answer is :"+sum);
-                   Toast.makeText(getApplicationContext(),"ans:"+sum,Toast.LENGTH_LONG).show();
+            String s = et1.getText().toString();
+            int n1 = Integer.parseInt(s);
 
+            Intent i = new Intent(MainActivity.this,Number2.class);
+            i.putExtra("Number1",n1);
+            startActivity(i);
 
-               } catch (Exception e) {
-                   e.printStackTrace();
-                   tv1.setText("enter a number");
-                   Toast.makeText(getApplicationContext(),"enter a number",Toast.LENGTH_LONG).show();
-               }
            }
        });
-        b2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent i = new  Intent(MainActivity.this,Calculator.class);
-                startActivity(i);
-                finish();
-            }
-        });
+
 
     }
 }
